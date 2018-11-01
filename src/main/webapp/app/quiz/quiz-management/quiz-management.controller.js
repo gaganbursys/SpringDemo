5c75e5adeac0897cdce2a40002f2a8f5dc728f22
Rohit <rohit.kumar2@bursysinfotech.com>
2018-11-01 18:08:32 +0530

    new field added in quiz page.

--- src/main/webapp/app/quiz/quiz-management/quiz-management.controller.js
+++ src/main/webapp/app/quiz/quiz-management/quiz-management.controller.js
@@ -9,7 +9,9 @@
 
     function QuizManagementController (filter) {
         var vm = this;
-        vm.quesCategoryArr =[ { "id":"1","category":"Math"},{ "id":2,"category":"JAVA" }, { "id":"3","category":"GK"}];
+        vm.quesCategoryArr =[ { "id":"1","category":"Math"},{ "id":"2","category":"JAVA" }, { "id":"3","category":"GK"}];
         vm.quesCategory = vm.quesCategoryArr[0];
+        vm.answerArr =[ { "id":1,"ans":"A"},{ "id":2,"ans":"B" }, { "id":3,"ans":"C"}, { "id":4,"ans":"D"}];
+        
     }
 })();
