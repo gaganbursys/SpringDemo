d0c2382204e000b2ac354031fc0134af1c700895
Rohit <rohit.kumar2@bursysinfotech.com>
2018-11-01 17:57:46 +0530

    Issue Fixed.

--- src/main/webapp/app/quiz/quiz-management/quiz-management.controller.js
+++ src/main/webapp/app/quiz/quiz-management/quiz-management.controller.js
@@ -5,21 +5,11 @@
         .module('springDemoApp')
         .controller('QuizManagementController', QuizManagementController);
 
-    QuizManagementController.$inject = ['$filter','QuizManagementService'];
+    QuizManagementController.$inject = ['$filter'];
 
-    function QuizManagementController (filter,QuizManagementService) {
+    function QuizManagementController (filter) {
         var vm = this;
-        vm.quesCategoryArr =[
-        	{
-        		id:1,category:"Math"
-        	},
-        	{
-        		id:2,category:"JAVA"
-        	},
-        	{
-        		id:3,category:"GK"
-        	}
-        	];
-        
+        vm.quesCategoryArr =[ { "id":"1","category":"Math"},{ "id":2,"category":"JAVA" }, { "id":"3","category":"GK"}];
+        vm.quesCategory = vm.quesCategoryArr[0];
     }
 })();
