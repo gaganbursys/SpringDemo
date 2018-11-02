758df0e4b4068b2bba1c0a311eb79c2c210bf524
Rohit <rohit.kumar2@bursysinfotech.com>
2018-11-02 12:55:15 +0530

    save Quiz functionality.

--- src/main/java/com/spring/demo/web/rest/QuizResource.java
+++ src/main/java/com/spring/demo/web/rest/QuizResource.java
@@ -3,10 +3,10 @@ package com.spring.demo.web.rest;
 import java.net.URISyntaxException;
 
 import javax.inject.Inject;
-import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,12 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codahale.metrics.annotation.Timed;
-import com.spring.demo.domain.User;
-import com.spring.demo.repository.QuizRepository;
 import com.spring.demo.security.AuthoritiesConstants;
 import com.spring.demo.service.QuizService;
 import com.spring.demo.service.dto.QuizDTO;
-import com.spring.demo.web.rest.errors.BadRequestAlertException;
 
 
 @RestController
@@ -31,116 +28,14 @@ public class QuizResource {
     
     @Inject
     private QuizService quizService;
-
-    @Inject
-    private QuizRepository quizRepository;
-    
-
-    /**
-     * POST  /users  : Creates a new user.
-     * <p>
-     * Creates a new user if the login and email are not already used, and sends an
-     * mail with an activation link.
-     * The user needs to be activated on creation.
-     *
-     * @param userDTO the user to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new user, or with status 400 (Bad Request) if the login or email is already in use
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     * @throws BadRequestAlertException 400 (Bad Request) if the login or email is already in use
-     */
+  
     @PostMapping("/quiz")
     @Timed
     @Secured(AuthoritiesConstants.ADMIN)
-    public ResponseEntity<User> createQuiz(@Valid @RequestBody QuizDTO quizDTO) throws URISyntaxException {
+    public ResponseEntity<QuizDTO> createQuiz( @RequestBody QuizDTO quizDTO) throws URISyntaxException {
         log.debug("REST request to save User : {}", quizDTO);
-
-        return null;       
+        return new ResponseEntity<>(quizService.saveQuiz(quizDTO),HttpStatus.OK);
+    
     }
     
-    
-    
-    
-    
-    
-
-   /* *//**
-     * PUT /users : Updates an existing User.
-     *
-     * @param userDTO the user to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated user
-     * @throws EmailAlreadyUsedException 400 (Bad Request) if the email is already in use
-     * @throws LoginAlreadyUsedException 400 (Bad Request) if the login is already in use
-     *//*
-    @PutMapping("/users")
-    @Timed
-    @Secured(AuthoritiesConstants.ADMIN)
-    public ResponseEntity<UserDTO> updateUser(@Valid @RequestBody UserDTO userDTO) {
-        log.debug("REST request to update User : {}", userDTO);
-        Optional<User> existingUser = userRepository.findOneByEmailIgnoreCase(userDTO.getEmail());
-        if (existingUser.isPresent() && (!existingUser.get().getId().equals(userDTO.getId()))) {
-            throw new EmailAlreadyUsedException();
-        }
-        existingUser = userRepository.findOneByLogin(userDTO.getLogin().toLowerCase());
-        if (existingUser.isPresent() && (!existingUser.get().getId().equals(userDTO.getId()))) {
-            throw new LoginAlreadyUsedException();
-        }
-        Optional<UserDTO> updatedUser = userService.updateUser(userDTO);
-
-        return ResponseUtil.wrapOrNotFound(updatedUser,
-            HeaderUtil.createAlert("userManagement.updated", userDTO.getLogin()));
-    }
-
-    *//**
-     * GET /users : get all users.
-     *
-     * @param pageable the pagination information
-     * @return the ResponseEntity with status 200 (OK) and with body all users
-     *//*
-    @GetMapping("/users")
-    @Timed
-    public ResponseEntity<List<UserDTO>> getAllUsers(Pageable pageable) {
-        final Page<UserDTO> page = userService.getAllManagedUsers(pageable);
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/users");
-        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
-    }
-
-    *//**
-     * @return a string list of the all of the roles
-     *//*
-    @GetMapping("/users/authorities")
-    @Timed
-    @Secured(AuthoritiesConstants.ADMIN)
-    public List<String> getAuthorities() {
-        return userService.getAuthorities();
-    }
-
-    *//**
-     * GET /users/:login : get the "login" user.
-     *
-     * @param login the login of the user to find
-     * @return the ResponseEntity with status 200 (OK) and with body the "login" user, or with status 404 (Not Found)
-     *//*
-    @GetMapping("/users/{login:" + Constants.LOGIN_REGEX + "}")
-    @Timed
-    public ResponseEntity<UserDTO> getUser(@PathVariable String login) {
-        log.debug("REST request to get User : {}", login);
-        return ResponseUtil.wrapOrNotFound(
-            userService.getUserWithAuthoritiesByLogin(login)
-                .map(UserDTO::new));
-    }
-
-    *//**
-     * DELETE /users/:login : delete the "login" User.
-     *
-     * @param login the login of the user to delete
-     * @return the ResponseEntity with status 200 (OK)
-     *//*
-    @DeleteMapping("/users/{login:" + Constants.LOGIN_REGEX + "}")
-    @Timed
-    @Secured(AuthoritiesConstants.ADMIN)
-    public ResponseEntity<Void> deleteUser(@PathVariable String login) {
-        log.debug("REST request to delete User: {}", login);
-        userService.deleteUser(login);
-        return ResponseEntity.ok().headers(HeaderUtil.createAlert( "userManagement.deleted", login)).build();
-    }*/
 }
--- src/main/webapp/app/quiz/quiz-management/quiz-management.controller.js
+++ src/main/webapp/app/quiz/quiz-management/quiz-management.controller.js
@@ -5,15 +5,24 @@
         .module('springDemoApp')
         .controller('QuizManagementController', QuizManagementController);
 
-    QuizManagementController.$inject = ['$filter','$http'];
+    QuizManagementController.$inject = ['$filter','$http' ];
 
-    function QuizManagementController (filter,$http) {
+    function QuizManagementController (filter,$http ) {
         var vm = this;
+        vm.quiz={};
         vm.quesCategoryArr =[ { "id":"1","category":"Math"},{ "id":"2","category":"JAVA" }, { "id":"3","category":"GK"}];
-        vm.quesCategory = vm.quesCategoryArr[0];
+        vm.quiz.category = vm.quesCategoryArr[0];
         vm.answerArr =[ { "id":1,"ans":"A"},{ "id":2,"ans":"B" }, { "id":3,"ans":"C"}, { "id":4,"ans":"D"}];
         
-        $http.post('')
+        vm.save = function(){
+        	console.log("data ",vm.quiz);
+        	vm.quiz.category = vm.quiz.category.id;
+        	vm.quiz.correctAns = vm.quiz.correctAns.id;
+        	  $http.post('api/quiz',vm.quiz).success(function(response){
+              	console.log("response.CorrectAns");
+              })
+        }
+      
         
     }
 })();
